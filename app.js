cf91e5f800894ede7abec2f699f15403c6ef7bf7
Pedro Wilson <pdro.wilson@gmail.com.brgit config --global user.emailgit config --global user.email pdro.wilson@gmail.com>
2019-09-08 18:22:24 -0300

    updated battle app

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ new Vue ({
             moveFour: 'Solar Beam'
         },
         playerPokemon: 'Bulbasaur',
-        opponentPokemon: 'Pidgey',
+        opponentPokemon: 'Mew Two',
         running: false,
         playerLife: 100,
         opponentLife: 100,
-        logs: ''
+        logs: [],
+        damages: []
     },
     computed: {
         hasResult() {
@@ -30,49 +31,55 @@ new Vue ({
             this.running = true
             this.playerLife = 100
             this.opponentLife = 100
-            this.log = []
+            this.logs = []
+            this.damages = []
+        },
+        run() {
+            this.running = false
+            this.logs = []
+            this.damages = []
         },
         attack(special, ult, source, move) {
             this.hurt('opponentLife', 2, 5, special, ult, this.playerPokemon, this.opponentPokemon, 'player')
             if(this.opponentLife > 0) {
                 this.hurt('playerLife', 8, 7, false, false, this.opponentPokemon, this.playerPokemon, 'opponent')
             }
-            this.moveLog(`${source} usou ${move}`)
+
+            this.moveDialog(`${source} used ${move}`)
         },
         hurt(prop, min, max, special, ult, source, target, cls) {
             const plusSpecial = special ? 6 : 0
             const plusUltimate = ult ? 150 : 0
             const hurt = this.getRandom(min + plusSpecial + plusUltimate, max + plusSpecial, plusUltimate)
             this[prop] = Math.max(this[prop] - hurt, 0)
-            this.battleLog(`${source} causou ${hurt} ao ${target}`)
             
+            this.hurtDialog(`${source} hits ${hurt} to ${target}`, cls)
         },
         drainAndHurt(source, move) {
-            this.heal(8, 6, this.playerPokemon)
-            this.hurt('playerLife', 8, 7, false, false, this.opponentPokemon, this.playerPokemon)
-            this.moveLog(`${source} usou ${move}`)
+            this.heal(8, 6, this.playerPokemon, this.opponentPokemon)
+            this.hurt('playerLife', 8, 7, false, false, this.opponentPokemon, this.playerPokemon, 'opponent')
+            
+            this.moveDialog(`${source} used ${move}`)
         },
         heal(min, max, source, target, cls) {
             const heal = this.getRandom(min, max)
             this.opponentLife = Math.ceil(Math.max(this.opponentLife - (heal / 8) , 0))
             this.playerLife = Math.min(this.playerLife + heal, 100)
-            this.healLog(`${source} curou ${heal}`)
-            
+
+            this.hurtDialog(`${source} drain ${heal} from ${target}`, 'player')
         },
         getRandom(min, max) {
             const value = Math.floor(Math.random() * min) + max
             return value
         },
-        battleLog(text) {
-            console.log(text)
+        moveDialog(moves) {
+            this.logs.unshift(moves)
+            console.log(this.logs[0])
         },
-        healLog(text) {
-            console.log(text)
-        },
-        moveLog(text) {
-            console.log(text)
+        hurtDialog(text, cls) {
+            this.damages.unshift({text, cls})
+            console.log(this.damages)
         }
-         
     },
     watch: {
         hasResult(value) {
